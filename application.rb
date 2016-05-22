ce985efa868e1ca45ea51b39e6d9dd3f8fbae027
Daniel Vartanov <dan@vartanov.net>
2016-05-22 15:53:21 +0100

    Загружаем файлы вручную, чтобы избежать проблемы зависимостей

--- application.rb
+++ application.rb
@@ -3,7 +3,11 @@ require 'http'
 require 'nokogiri'
 require 'active_record'
 
-Dir[File.expand_path "app/**/*.rb"].each { |file| require_relative(file) }
+require_relative 'app/news'
+require_relative 'app/snapshot'
+require_relative 'app/news_data'
+require_relative 'app/agencies/agency'
+require_relative 'app/agencies/knews'
 
 ActiveRecord::Base.logger = Logger.new(STDOUT)
 ActiveRecord::Base.establish_connection YAML::load(File.open('db/config.yml'))['development']
