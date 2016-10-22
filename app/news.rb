ba0e4cc21fe7fbd891aaf7c55dea57d92ce5f6e5
Daniel Vartanov <dan@vartanov.net>
2016-10-22 16:21:46 +0100

    Добавил скоупы, методы и ещё много чего в модель News

--- app/news.rb
+++ app/news.rb
@@ -1,3 +1,9 @@
 class News < ActiveRecord::Base
-  has_many :snapshots
+  has_many :snapshots, -> { order(:updated_at) }, dependent: :destroy
+
+  scope :edited, -> { joins(:snapshots).group('snapshots.news_id, news.id').having('count(*) > 1') }
+
+  def title_edited?
+    snapshots.map(&:title).uniq.size > 1
+  end
 end
