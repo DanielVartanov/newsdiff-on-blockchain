040de5f84ade900846e6dc8cdbe42641ff02f4be
puzanov <puzanov@gmail.com>
2017-06-08 14:35:24 +0600

    added exception handling on news fetching

--- scraper/scripts/parse_and_save_diffs_in_html.rb
+++ scraper/scripts/parse_and_save_diffs_in_html.rb
@@ -36,15 +36,19 @@ agencies = [Agencies::Azattyq.new,
 loop do
   agencies.each do |agency|
     agency.news.each do |news_data|
-      news = News.find_or_create_by! agency: news_data.agency, remote_id: news_data.remote_id
-
-      unless news.snapshots.exists? checksum: news_data.checksum
-        news.snapshots.create! checksum: news_data.checksum,
-                               url: news_data.url,
-                               title: news_data.title,
-                               author: news_data.author,
-                               published_at: news_data.published_at,
-                               content: news_data.content
+      begin
+        news = News.find_or_create_by! agency: news_data.agency, remote_id: news_data.remote_id
+        unless news.snapshots.exists? checksum: news_data.checksum
+          news.snapshots.create! checksum: news_data.checksum,
+                                 url: news_data.url,
+                                 title: news_data.title,
+                                 author: news_data.author,
+                                 published_at: news_data.published_at,
+                                 content: news_data.content
+        end
+      rescue Exception => e
+        puts e.message
+        puts e.backtrace.inspect
       end
     end
   end
