369ac31cba10a7e3da3bea0d83db012b807a569c
puzanov <puzanov@gmail.com>
2017-08-06 18:13:06 +0600

    sep title

--- scraper/scripts/parse_and_save_diffs_in_html.rb
+++ scraper/scripts/parse_and_save_diffs_in_html.rb
@@ -56,6 +56,7 @@ loop do
   html = '
   <html>
   <head>
+      <title>Newsdiff -- Detects edited news, stores proof on blockchain</title>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, user-scalable=0">
       <meta name="format-detection" content="telephone=no">
