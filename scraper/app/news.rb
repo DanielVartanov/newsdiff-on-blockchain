77c01ed4e68f021f8be9af95d9e59d1f865fd8cc
puzanov <puzanov@gmail.com>
2018-01-28 18:24:11 +0600

    + added helper docker script for dev cases * added custom columns selection to has_many :snapshots relation

--- scraper/app/news.rb
+++ scraper/app/news.rb
@@ -1,6 +1,5 @@
 class News < ActiveRecord::Base
-  has_many :snapshots, -> { order(:updated_at) }, dependent: :destroy
-
+  has_many :snapshots, -> { select(:id, :news_id, :checksum, :url, :title, :author, :published_at, :created_at, :updated_at).order(:updated_at) }, dependent: :destroy
   scope :edited, -> { joins(:snapshots).select("news.*, snapshots.news_id").group('snapshots.news_id, news.id').having('count(*) > 1') }
 
   def title_edited?
---
+++ scraper/docker-down-build-up.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+docker-compose down
+docker-compose down
+docker-compose build
+docker-compose up
